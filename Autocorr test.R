51342c92288da031e4c25dc7d25c0dc614cb6bcf
kmgreco <kaitlin.m.greco@stonybrook.edu>
2014-05-15 20:12:26 -0400

    Autocorr

---
+++ Autocorr test.R
@@ -0,0 +1,11 @@
+time <- 1:25
+sig <- 1
+x <- rnorm(length(time) - 1, 0, sd = sqrt(sig))
+y <- c(0, cumsum(x)) + 100
+
+plot(time, z, type = 'l')
+
+z <- diffinv(rnorm(99))
+
+z <- filter(rnorm(25, 100), filter = rep(1, 15), circular = TRUE)
+plot(time, z, type = 'l')
